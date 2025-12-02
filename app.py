efd86c90b9b3d1c5e03436e583fcf54b22935585
hoverholly <hoverhollydev@gmail.com>
2025-12-01 20:37:50 -0500

    cu-ms-payments

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+API simple que responde Hola Mundo en el puerto 3000.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return "Hola Mundo", 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3000, debug=False)
