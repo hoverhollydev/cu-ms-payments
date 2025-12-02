2bcc1bc95eb09fceb08b46766f63ccf544cd5c05
hoverholly <hoverhollydev@gmail.com>
2025-12-01 20:45:25 -0500

    cu-ms-payments

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return "Hola Mundo", 200
+    return "Hola Mundo cu-ms-payments", 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000, debug=False)
